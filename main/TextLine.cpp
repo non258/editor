2cc83eb70f997fb9dc44511603f76a19d00229e2
non258 <c0117037bf@edu.teu.ac.jp>
2018-07-21 14:39:01 +0900

    TextLineの作成

---
+++ main/TextLine.cpp
@@ -0,0 +1,12 @@
+#include <random>
+#include <ncurses.h>
+#include "MyCursor.hpp"
+#include "Frame.hpp"
+#include "Window.hpp"
+
+using namespace std;
+
+TextLine::TextLine(double x, double y) : lineX(x), lineY(y)
+{
+
+}
---
+++ main/TextLine.hpp
@@ -0,0 +1,14 @@
+#ifndef TEXTLINE_HEADER
+#define TEXTLINE_HEADER
+
+class TextLine 
+{
+  public:
+    double lineX;
+    double lineY;
+    vector<char> st;
+  TextLine(double x, double y);
+};
+
+#endif
+
